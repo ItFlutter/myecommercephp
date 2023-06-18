67b51c5acc91e034a37302de2ee6e1b1e9515d60
ahmad omran <ahmadomran921@gmail.com>
2023-06-18 21:40:46 +0300

    seven commit

--- view.sql
+++ view.sql
@@ -6,8 +6,8 @@ INNER JOIN categories ON items.items_cat=categories.categories_id;
 
 
 
-CREATE OR REPLACE VIEW myfavorite
-SELECT favorite.*,items.*,users.users_id FROM favorite
+CREATE OR REPLACE VIEW myfavorite as
+SELECT favorite.*,items.*,users.users_id,items.items_price -(items.items_price*(items.items_discount/100)) as itemspricediscount FROM favorite
 INNER JOIN items ON favorite.favorite_itemsid=items.items_id
 INNER JOIN users ON favorite.favorite_usersid=users.users_id
 
